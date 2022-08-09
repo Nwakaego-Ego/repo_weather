e4f3e05187d76ef3bb7b782c82d2fb5f8967a8a4
Nwakaego Glory Nwaekpe <glorynwaekpe@gmail.com>
2022-08-09 21:55:36 +0200

    Weather git commit

--- main.js
+++ main.js
@@ -1,86 +1,77 @@
-function formatDate(date) {
-  let hours = date.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  let minutes = date.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-
-  let dayIndex = date.getDay();
+function time(currentTime) {
   let days = [
     "Sunday",
     "Monday",
     "Tuesday",
-    "Wednesday",
+    "Wednessday",
     "Thursday",
     "Friday",
     "Saturday",
   ];
-  let day = days[dayIndex];
+  let day = days[currentTime.getDay()];
 
-  return `${day} ${hours}:${minutes}`;
+  let hour = currentTime.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+
+  let minutes = currentTime.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let date = document.getElementById("date");
+  date.innerHTML = `${day} ${hour} : ${minutes}`;
 }
 
-function displayWeatherCondition(response) {
-  document.querySelector("#city").innerHTML = response.data.name;
-  document.querySelector("#temperature").innerHTML = Math.round(
-    response.data.main.temp
-  );
-
-  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-  document.querySelector("#wind").innerHTML = Math.round(
-    response.data.wind.speed
-  );
-  document.querySelector("#description").innerHTML =
-    response.data.weather[0].main;
-}
-
-function searchCity(city) {
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayWeatherCondition);
-}
-
-function handleSubmit(event) {
-  event.preventDefault();
-  let city = document.querySelector("#city-input").value;
-  searchCity(city);
-}
-
-function searchLocation(position) {
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
-
-  axios.get(apiUrl).then(displayWeatherCondition);
-}
-
-function getCurrentLocation(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(searchLocation);
-}
-
-function convertToFahrenheit(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = 66;
-}
-
-function convertToCelsius(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = 19;
-}
-
-let dateElement = document.querySelector("#date");
+// let second = currentTime.getSeconds();
 let currentTime = new Date();
-dateElement.innerHTML = formatDate(currentTime);
+time(currentTime);
 
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", handleSubmit);
+let search = document.getElementById("search-form");
+search.addEventListener("submit", searchBtn);
 
-let currentLocationButton = document.querySelector("#current-location-button");
-currentLocationButton.addEventListener("click", getCurrentLocation);
+let apiKey = "0af0ba164e313d2bc52c846cbc253f06";
+let city = "ghana";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
-searchCity("New York");
+function searchBtn(event) {
+  event.preventDefault();
+  let cityInput = document.getElementById("city-input");
+  let city = document.getElementById("city");
+  city.innerHTML = cityInput.value;
+
+  // axios.get(apiUrl).then((response) => { let temperature = document.getElementById("temperature");
+  // temperature.innerHTML = Math.round(response.data.main.temp)});
+  axios.get(apiUrl).then(weatherSearch);
+}
+
+function weatherSearch(response) {
+  // console.log(response);
+  let temperature = document.getElementById("temperature");
+  temperature.innerHTML = Math.round(response.data.main.temp);
+  console.log(response.data);
+}
+
+// let fahrenheit = document.getElementById("fahrenheit-link");
+// fahrenheit.addEventListener("click", fahrenheitBtn);
+
+// let celsius = document.getElementById("celsius-link");
+// celsius.addEventListener("click", celsiusBtn);
+
+// function fahrenheitBtn() {
+//   let temperature = document.getElementById("temperature");
+//   // temperature = Number(temperature);
+//   // temperature.innerHTML = (temperature * 9) / 5 + 32;
+//   temperature.innerHTML = (temperature.innerHTML * 9) / 5 + 32;
+//   // temperature.innerHTML = 10;
+//   console.log(temperature);
+// }
+
+// function celsiusBtn() {
+//   let temperature = document.getElementById("temperature");
+//   temperature = Number(temperature);
+//   temperature = ((temperature - 32) * 5) / 9;
+//   // temperature.innerHTML = 10;
+//   console.log(temperature);
+// }
